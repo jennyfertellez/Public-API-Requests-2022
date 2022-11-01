00afa8e76c1a855a564686c661c9f2bb866a91f6
jennyfertellez <102021487+jennyfertellez@users.noreply.github.com>
2022-11-01 16:12:10 -0700

    Updated modal and closing button

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 //Global Variables
 const galleryContainer = document.querySelector('.search-container');
 const gallery = document.getElementById('gallery');
-const modalContainer = document.querySelector(".modal-content");
-const modalClose = document.querySelector(".modal-close");
+const modalContainer = document.querySelector(".modal-container");
+const modalClose = document.querySelector(".modal-close-btn");
 let employeeData = [];
 let card = document.querySelector('.card');
 
@@ -32,10 +32,9 @@ const userData = (usersInfo) => {
          </div>
         `
         gallery.insertAdjacentHTML('beforeend', userHTML);
-})
-   
-    console.log(employeeData)
+    })
 }
+
 // FETCH requests data from Random User API
 fetch('https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US')
     .then((response) => response.json())
@@ -44,31 +43,47 @@ fetch('https://randomuser.me/api/?results=12&inc=name, picture, email, location,
     .catch(err => console.log(err))
 
 //Create a Modal Window
-//const index = card.getAttribute('data-index')
-function displayModal (index) {
-    
-    let {name, dob, phone, email, location: {city, street, state, postcode}, picture } = employees[index];
-    
+function displayModal(index) {
+//Using object destructuring to make cleaner object literals
+    let {name, dob, phone, email, location: {city, street, state, postcode}, picture } = employeeData[index];
+//Date Setup to ensure date is done in format 
     let date = new Date(dob.date);
     date.toLocaleDateString();
+    const month = String(date.getMonth() +1).padStart(2, '0');
 
+//Captured information for Module Window
     const modalInfo = document.querySelector('.modal-info-container');
     modalInfo.innerHTML = '';
 
-    const modalHTML=`
+    const modalHTML = `
                 <img class="modal-img" src="${picture.medium}" alt="profile picture">
+                <div class="modal-info-container">
                 <h3 id="name" class="modal-name cap">${name.first} ${name.last}</h3>
                 <p class="modal-text">${email}</p>
                 <p class="modal-text cap">${city}, ${state}</p>
                 <hr>
                 <p class="modal-text">${phone}</p>
                 <p class="modal-text">${street.number} ${street.name}, ${city}, ${state}, ${postcode}  </p>
-                <p Birthday: ${date.getDate()}/${date.getFullYear()}</p>
+                <p class="modal-text">Birthday: ${month}/${date.getDate()}/${date.getFullYear()}</p>
             </div>
         </div>
         </div>`;
 
     modalInfo.innerHTML = modalHTML;
     }
+//Event Listener to listen when user clicks on employee card
+    gallery.addEventListener('click', e => {
+        if (e.target !== gallery) {
+
+            const card = e.target.closest(".card");
+            const index = card.getAttribute('data-index')
+            displayModal(index);
+            modalContainer.style.display = 'block';
+        }
+    });
 
 //Close the Modal Window
+//Event Listener to listen when user clicks on the "x"
+modalClose.addEventListener('click', () => {
+    modalContainer.style.display = 'none';
+});
