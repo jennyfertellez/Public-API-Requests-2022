ee17deba9f58cb6b8830c8b69fca538594a6a281
jennyfertellez <102021487+jennyfertellez@users.noreply.github.com>
2022-11-01 14:17:15 -0700

    changed userData

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
+//Global Variables
 const galleryContainer = document.querySelector('.search-container');
 const gallery = document.getElementById('gallery');
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
 let employeeData = [];
+let card = document.querySelector('.card');
 
 //Gallery Container
 galleryContainer.innerHTML = `
@@ -9,55 +13,68 @@ galleryContainer.innerHTML = `
     <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
 </form> `;
 
-// FETCH requests data from Random User API
-fetch('https://randomuser.me/api/?results=12&nat=US')
-    .then((response) => response.json())
-    .then(data => {
-        employeeData = data.results
-    })
-    .then(() => userData(employeeData))
 
 //Display 12 Random Users
-function userData(data) {
-    for (let i = 0; i < data.length; i++){
-        const userCard =
-        `<div class = "card">
+const userData = (usersInfo) => {
+
+    let userHTML = '';
+
+    usersInfo.forEach((user, index) => {
+        employeeData.push(user);
+        let name = user.name;
+        let email = user.email;
+        let city = user.location.city;
+        let state = user.location.state;
+        let picture = user.picture;
+
+        userHTML +=`
+        <div class = "card" data-index = "${index}">
             <div class="card-img-container">
-             <img class="card-img" src="${data[i].picture.medium}" alt="profile picture">
-    </div>
-    <div class="card-info-container">
-        <h3 id="name" class="card-name cap">${data[i].name.first} ${data[i].name.last}</h3>
-        <p class="card-text">${data[i].email}</p>
-        <p class="card-text cap">${data[i].location.city} ${data[i].location.state}</p>
-    </div>`;
-
-    gallery.insertAdjacentHTML('beforeend', userCard);
-
-
-    }
+            <img class="card-img" src="${picture.medium}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+        <h3 id="name" class="card-name cap">${name.first} ${name.last}</h3>
+        <p class="card-text">${email}</p>
+        <p class="card-text cap">${city} ${state}</p>
+         </div>
+        `
+})
+    gallery.innerHTML = userHTML;
+    console.log(employeeData)
 }
+// FETCH requests data from Random User API
+fetch('https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US')
+    .then((response) => response.json())
+    .then(response => response.results)
+    .then(userData)
+    .catch(err => console.log(err))
 
 //Create a Modal Window
-function displayModal (data) {
-    console.log(employeeData)
-    const modalContainer=
-    `<div class = "modal-container">
-        <div class="modal">
-            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-            <div class="modal-info-container">
-                <img class="modal-img" src="${data.picture.medium}" alt="profile picture">
-                <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
-                <p class="modal-text">${data.email}</p>
-                <p class="modal-text cap">${data.location.city}, ${data.location.state}</p>
+//const index = card.getAttribute('data-index')
+function displayModal (index) {
+    
+    let {name, dob, phone, email, location: {city, street, state, postcode}, picture } = employees[index];
+    
+    let date = new Date(dob.date);
+    date.toLocaleDateString();
+
+    const modalInfo = document.querySelector('.modal-info-container');
+    modalInfo.innerHTML = '';
+
+    const modalHTML=`
+                <img class="modal-img" src="${picture.medium}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${name.first} ${name.last}</h3>
+                <p class="modal-text">${email}</p>
+                <p class="modal-text cap">${city}, ${state}</p>
                 <hr>
-                <p class="modal-text">${data.cell}</p>
-                <p class="modal-text">${data.location.street.number} ${data.location.street.name}, ${data.location.city}, ${data.location.state}, ${data.location.postCode}  </p>
-                <p class="modal-text">${data.dob.date}</p>
+                <p class="modal-text">${phone}</p>
+                <p class="modal-text">${street.number} ${street.name}, ${city}, ${state}, ${postcode}  </p>
+                <p Birthday: ${date.getDate()}/${date.getFullYear()}</p>
             </div>
         </div>
         </div>`;
 
-    gallery.insertAdjacentHTML('beforeend', modalContainer);
+    modalInfo.innerHTML = modalHTML;
     }
 
 //Close the Modal Window
